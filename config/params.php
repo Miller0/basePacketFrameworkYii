f09c32b38dc024c53c7719fd86d418d9a0851866
Miller0 <mr.milller2017.11@gmail.com>
2020-07-23 01:29:04 +0300

    *

--- config/params.php
+++ config/params.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'adminEmail' => 'admin@spacebot.pp.ua',
-    'senderEmail' => 'noreply@spacebot.pp.ua',
+    'adminEmail' => 'robot@spacebot.pp.uaa',
+    'senderEmail' => 'robot@spacebot.pp.ua',
     'senderName' => 'spacebot.pp.ua',
     'user.passwordResetTokenExpire' => 3600,
     'supportEmail' => 'robot@spacebot.pp.ua'

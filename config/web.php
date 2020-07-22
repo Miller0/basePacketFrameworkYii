c3dde2147dee181b1591342d8a0e001d7d176aae
Miller0 <mr.milller2017.11@gmail.com>
2020-07-22 16:01:05 +0300

    *

--- config/web.php
+++ config/web.php
@@ -5,7 +5,6 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
-    'baseUrl'=> '',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'aliases' => [
@@ -15,6 +14,7 @@ $config = [
     'components' => [
         'request' => [
             'cookieValidationKey' => 'test',
+            'baseUrl'=> '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
